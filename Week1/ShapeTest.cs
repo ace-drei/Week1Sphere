051a7ebe70211e1a6b0d1c591f421a6feab185a0
Dre <aciobirca97@gmail.com>
2024-01-05 17:31:30 +0000

    Added week1

---
+++ Week1/ShapeTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Shapes;
+
+class ShapeTest
+{
+    public static void Main()
+    {
+        Sphere sphere = new Sphere(5);
+        Sphere sphere1 = new Sphere(-5);
+
+        Console.WriteLine(sphere);
+        Console.WriteLine(sphere1);
+
+    }
+}
---
+++ Week1/Shapes.cs
@@ -0,0 +1,94 @@
+﻿using System;
+namespace Shapes;
+
+
+public abstract class ThreeDShape
+{
+    string shapeType;
+
+    //public read only property
+    public string ShapeType
+    {
+        get { return shapeType; }
+    }
+
+    //Constructor
+    public ThreeDShape(string type)
+    {
+        shapeType = type;
+    }
+
+    ~ThreeDShape()
+    {
+        Console.WriteLine("Called the Deconstructor");
+    }
+
+    //Abstract method
+    public abstract double volume();
+
+
+}
+
+
+
+public class Sphere : ThreeDShape
+{
+    //field to store radius
+    private double radius;
+
+    //Making sure Radius is postive
+    public double Radius
+    {
+        get { return radius; }
+        set
+        {
+            if (value > 0)
+                radius = value;
+            else
+                throw new ArgumentException("Radius must be positive.");
+        }
+    }
+
+    public Sphere(double radius) : base("Sphere")
+    {
+        this.Radius = radius;
+    }
+
+
+
+    //overrides volume
+    public override double volume()
+    {
+        return Math.PI * radius * radius * radius;
+    }
+
+
+    //Creates constructor in Sphere 
+  
+    public override string ToString()
+    {
+        return  "Radius = " + radius + " \nCalculate volume =  " + volume();
+    }
+
+}
+
+class ShapeTest
+{
+    public static void Main()
+    {
+        Sphere sphere = new Sphere(5);
+        Sphere sphere1 = new Sphere(-5);
+
+        Console.WriteLine(sphere);
+        Console.WriteLine(sphere1);
+
+    }
+}
+
+
+
+interface IHasVolume
+{
+    void Volume(double volume);
+}
+
